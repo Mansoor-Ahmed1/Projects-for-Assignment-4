c4d4ccf3533e61154b66102037523087131a7b9e
unknown <mansoor.ahmed11521@gmail.com>
2025-05-28 05:09:53 +0500

    done with user guessing task

--- Projects to be submitted by Students/user_guess_the_number.py
+++ Projects to be submitted by Students/user_guess_the_number.py
@@ -1,4 +1,25 @@
 import random
 
+score = 10
 num = random.randint(1,100)
 print(num)
+def main():
+    global score
+    while(score>0):
+        user_guess = int(input(f"Guess a Number between 1 and 100!"))         
+        if user_guess == num:
+            print(f"""Congrats, You got that right!
+                  You obtained {score} points out of 10.""")
+            break
+        elif user_guess > num:
+            score -= 1  
+            print(f"Your guessed number is Greater, {score} Attempts left.")
+    
+        elif user_guess < num:
+            score -= 1
+            print(f"Your guessed number is Lesser, {score} Attempts left.")
+    else:
+        print(f"Game over! You've used all your attempts. The number was {num}.")
+        
+if __name__ == "__main__":
+    main()
